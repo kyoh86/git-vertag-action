9d0260ed291501c2665c9f0228d9158bf5c6993f
Kyoichiro Yamada <me@kyoh86.dev>
2020-11-23 18:03:59 +0900

    Support "push" option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-git-vertag --fetch "${1}"
+method="${1}"
+push="${2}"
+
+if [ "${push}" != "true" ]; then
+  push=""
+fi
+
+git-vertag --fetch "${method}" ${push:+"--push-to origin"}
+
 echo "::set-output name=vertag::$(git-vertag get)"
