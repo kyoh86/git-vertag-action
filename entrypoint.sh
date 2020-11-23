e96e595902745a3cf7ae6b545991376d0ccace07
Kyoichiro Yamada <me@kyoh86.dev>
2020-11-23 17:13:08 +0900

    wip: implement "push" option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,17 @@
 
 set -e
 
-git-vertag --fetch "${1}"
+method="${1}"
+
+push="${2}"
+if [ "${push}" = "true" ]; then
+  push_opt1="--push-to"
+  push_opt2="origin"
+else
+  push_opt1=""
+  push_opt2=""
+fi
+
+git-vertag --fetch "${method}" "${push_opt1}" "${push_opt2}"
+
 echo "::set-output name=vertag::$(git-vertag get)"
